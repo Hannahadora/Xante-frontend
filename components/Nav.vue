4bf9d0730686ff60f75884518f5150e9d05260db
Hannah Ejimofor <hejimofor141@uniport.edu.ng>
2021-07-29 22:02:56 +0100

    ui setup

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="top-nav">
-      <ul class="flex items-center">
-          <li class="top-nav-link">Home</li>
-          <li class="top-nav-link">Compare</li>
-          <li class="top-nav-link">Market Analyst</li>
-          <li class="top-nav-link">Tools</li>
-          <li class="top-nav-link">Opportunities</li>
-          <li class="top-nav-link">Media</li>
-          <li class="top-nav-link">Contact Us</li>
+      <ul class="flex items-center gap-10">
+          <NuxtLink to="/" class="top-nav-link">Home</NuxtLink>
+          <NuxtLink to="/compare" class="top-nav-link">Compare</NuxtLink>
+          <NuxtLink to="/marketAnalytics" class="top-nav-link">Market Analyst</NuxtLink>
+          <NuxtLink to="/tools" class="top-nav-link">Tools</NuxtLink>
+          <NuxtLink to="/opportunities" class="top-nav-link">Opportunities</NuxtLink>
+          <NuxtLink to="/media" class="top-nav-link">Media</NuxtLink>
+          <NuxtLink to="/contact" class="top-nav-link">Contact Us</NuxtLink>
       </ul>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Nav',
 
 }
 </script>
 
 <style>
+  .top-nav-link {
+    color: #DCE0E9;
+    /* margin-right: 40px;  */
+    font-size: 16px;
+    font-weight: 300;
+  }
+
+  .top-nav-link-exact {
+    font-weight: 600;
+  }
   
 </style>
