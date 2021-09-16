ef92e0f595e11cc537f33a52159185084d8c675a
LizzyKate <onyenekwelizabeth@gmail.com>
2021-09-16 02:55:10 +0100

    fixed nav

--- components/Header.vue
+++ components/Header.vue
@@ -1,39 +1,40 @@
 <template>
   <div class="header flex items-center justify-between">
-      <img class="img2 xl:hidden block" src="../assets/images/Menu 2.png" alt="">  
-      <Logo />
-      <Nav class="xl:block hidden" />  
-      <PryButton text="Create Account" />
+    <img
+      class="img2 xl:hidden block"
+      src="../assets/images/Menu 2.png"
+      alt=""
+    />
+    <Logo />
+    <Nav class="xl:block hidden" />
+    <PryButton text="Create Account" />
   </div>
 </template>
 
 <script>
-import PryButton from './Buttons/PryButton.vue'
-import Logo from './Logo.vue'
-import Nav from './Nav.vue'
+import PryButton from "./Buttons/PryButton.vue";
+import Logo from "./Logo.vue";
+import Nav from "./Nav.vue";
 export default {
   components: { Logo, Nav, PryButton },
-
-}
+};
 </script>
 
 <style>
-    .header {
-        height: 80px;
-        width: 100%;
-        border-radius: 0px;
-        background: #0B0F1A;
-        padding: 14px 120px;
-        position: fixed;
-        top: 0;
-        z-index: 999999.99999;
-    }
+.header {
+  height: 80px;
+  width: 100%;
+  border-radius: 0px;
+  background: #0b0f1a;
+  padding: 14px 120px;
+  position: fixed;
+  top: 0;
+  z-index: 100000000;
+}
 
-     @media screen and (max-width: 768px) {
-        .header {
-            padding: 24px;
-        }
-
-    }
-
+@media screen and (max-width: 768px) {
+  .header {
+    padding: 24px;
+  }
+}
 </style>
